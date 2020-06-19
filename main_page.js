53f44e68d232d1318048d81baf0108b256f28e4c
Dheeraj Kumar <dheeraj.kumar@ucalgary.ca>
2020-06-18 23:02:05 -0600

    website now responsive

---
+++ main_page.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  $('.menu-toggle').click(function(){
+    $('nav').toggleClass('active');
+  })
+  $('ul li').click(function(){
+    $(this).siblings().removeClass('active');
+    $(this).toggleClass('active');
+  })
+})
+
+
+
+function checkWidth(init)
+{
+    /*If browser resized, check width again */
+    if ($(window).width() < 1290) {
+        $('ul li').removeClass('fronthover');
+    }
+    else{
+      $('ul li').addClass('fronthover')
+    }
+    
+}
+
+$(document).ready(function() {
+    checkWidth(true);
+
+    $(window).resize(function() {
+        checkWidth(false);
+    });
+});
